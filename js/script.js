b0664847f780662599ed9d3a8c00d7c1258b9c03
seiya1185 <s.takahashi0981@gmail.com>
2021-03-29 03:42:59 +0900

    fix

--- js/script.js
+++ js/script.js
@@ -1,93 +1,93 @@
-  // ----- ヘッダーのサイズを変更する -----
+// ----- ヘッダーのサイズを変更する -----
 
-  $(function() {
-    $(window).scroll(function(){
-      if($(window).scrollTop() > 100) {
-        $(".header__inner").addClass("small");
-      }else{
-        $(".header__inner").removeClass("small");
-      }
-    });
-    $(window).scroll(function(){
-      if($(window).scrollTop() > 100) {
-        $(".header__inner__logo").addClass("small");
-      }else{
-        $(".header__inner__logo").removeClass("small");
-      }
-    });
-    $(window).scroll(function(){
-      if($(window).scrollTop() > 100) {
-        $(".nav__list").addClass("small");
-      }else{
-        $(".nav__list").removeClass("small");
-      }
-    });
+$(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+      $(".header__inner").addClass("small");
+    } else {
+      $(".header__inner").removeClass("small");
+    }
   });
-
-  // ----- ヘッダーのサイズを変更する -----
-
-
-  // ページ内リンクの設定 --------
-
-  $(function() {
-    $('a[href*="#"]').click(function() {
-
-      var target = $($(this).attr("href")).offset().top;
-
-      target -= 50;
-
-      $("html, body").animate({scrollTop: target}, 500);
-
-      return false;
-    });
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+      $(".header__inner__logo").addClass("small");
+    } else {
+      $(".header__inner__logo").removeClass("small");
+    }
   });
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+      $(".nav__list").addClass("small");
+    } else {
+      $(".nav__list").removeClass("small");
+    }
+  });
+});
 
-  // ------ ページ内リンク ------
+// ----- ヘッダーのサイズを変更する -----
 
+// ページ内リンクの設定 --------
 
-  // スライドイン ---------
+$(function () {
+  $('a[href*="#"]').click(function () {
+    var target = $($(this).attr("href")).offset().top;
 
-  // PC版のみ上からスライド
+    target -= 50;
+
+    $("html, body").animate({ scrollTop: target }, 500);
+
+    return false;
+  });
+});
+
+// ------ ページ内リンク ------
+
+// スライドイン ---------
+
+// PC版のみ上からスライド
 if (!navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
   $(function () {
     $(window).scroll(function () {
-        const wHeight = $(window).height();
-        const scrollAmount = $(window).scrollTop();
-        const delayTime = 500;
-        $('.slideX').each(function(i) {
-            const targetPosition = $(this).offset().top;
-            if(scrollAmount > targetPosition - wHeight + 60) {
-              $(this).delay(i * delayTime).queue(()=> {
-                $(this).addClass("fadeInDown").dequeue();
-              });
-            }
-        });
+      const wHeight = $(window).height();
+      const scrollAmount = $(window).scrollTop();
+      const delayTime = 500;
+      $(".slideX").each(function (i) {
+        const targetPosition = $(this).offset().top;
+        if (scrollAmount > targetPosition - wHeight + 60) {
+          $(this)
+            .delay(i * delayTime)
+            .queue(() => {
+              $(this).addClass("fadeInDown").dequeue();
+            });
+        }
+      });
     });
-});
+  });
 }
 
 // スマホ版のみ横からスライド
 if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
   $(function () {
     $(window).scroll(function () {
-        const wHeight = $(window).height();
-        const scrollAmount = $(window).scrollTop();
-        const delayTime = 500;
-        $('.slideX').each(function(i) {
-            const targetPosition = $(this).offset().top;
-            if(scrollAmount > targetPosition - wHeight + 60) {
-              $(this).delay(i * delayTime).queue(()=> {
-                $(this).addClass("fadeInDown").dequeue();
-                $(this).addClass("slide-left").dequeue();
-              });
-            }
-        });
+      const wHeight = $(window).height();
+      const scrollAmount = $(window).scrollTop();
+      const delayTime = 500;
+      $(".slideX").each(function (i) {
+        const targetPosition = $(this).offset().top;
+        if (scrollAmount > targetPosition - wHeight + 60) {
+          $(this)
+            .delay(i * delayTime)
+            .queue(() => {
+              $(this).addClass("fadeInDown").dequeue();
+              $(this).addClass("slide-left").dequeue();
+            });
+        }
+      });
     });
-});
+  });
 }
 
-  // スライドインここまで --------
-
+// スライドインここまで --------
 
 $(function () {
   //ハンバーガーメニュー
@@ -117,46 +117,49 @@ $(function () {
   });
 });
 
-$('.slider').slick({
-  autoplay: false,//自動的に動き出すか。初期値はfalse。
-  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-  speed: 500,//スライドのスピード。初期値は300。
-  slidesToShow: 3,//スライドを画面に3枚見せる
-  slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-  centerMode: true,//要素を中央ぞろえにする
-  variableWidth: true,//幅の違う画像の高さを揃えて表示
-  dots: true,//下部ドットナビゲーションの表示
+$(".slider").slick({
+  autoplay: false, //自動的に動き出すか。初期値はfalse。
+  infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+  speed: 500, //スライドのスピード。初期値は300。
+  slidesToShow: 3, //スライドを画面に3枚見せる
+  slidesToScroll: 1, //1回のスクロールで1枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
+  centerMode: true, //要素を中央ぞろえにする
+  variableWidth: true, //幅の違う画像の高さを揃えて表示
+  dots: true, //下部ドットナビゲーションの表示
 });
-
 
 // テーブルエルのハイライト ---------
 
-$(function() {
+$(function () {
   // セルをマウスオーバー
-  $("td").hover(function() {
-    $(this).parent().addClass("target");
-  }, function() {
-    $(".target").removeClass("target");
-  });
+  $("td").hover(
+    function () {
+      $(this).parent().addClass("target");
+    },
+    function () {
+      $(".target").removeClass("target");
+    }
+  );
 });
 
 // テーブルセルのハイライトここまで ---------
 
 $(function () {
   $(window).scroll(function () {
-      const wHeight = $(window).height();
-      const scrollAmount = $(window).scrollTop();
-      const delayTime = 200;
-      $('.slideY').each(function(i) {
-          const targetPosition = $(this).offset().top;
-          if(scrollAmount > targetPosition - wHeight + 60) {
-            $(this).delay(i * delayTime).queue(()=> {
-              $(this).addClass("fadeInDown").dequeue();
-            });
-          }
-        });
+    const wHeight = $(window).height();
+    const scrollAmount = $(window).scrollTop();
+    const delayTime = 200;
+    $(".slideY").each(function (i) {
+      const targetPosition = $(this).offset().top;
+      if (scrollAmount > targetPosition - wHeight + 60) {
+        $(this)
+          .delay(i * delayTime)
+          .queue(() => {
+            $(this).addClass("fadeInDown").dequeue();
+          });
+      }
     });
+  });
 });
-
